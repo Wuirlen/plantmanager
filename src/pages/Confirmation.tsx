fe4d3fc3fb89e3a5d8436ac8784fa3c7417ee33b
Wuirlen Azevedo <wuirlen.azevedo@gmail.com>
2021-04-27 16:57:42 -0400

    Atualizando

--- src/pages/Confirmation.tsx
+++ src/pages/Confirmation.tsx
@@ -1,10 +1,18 @@
 import React, { useState } from 'react';
+import { useNavigation } from '@react-navigation/core';
 import { SafeAreaView, StyleSheet, View, Text } from 'react-native';
 import { Button } from '../components/Button';
 import colors from '../styles/colors';
 import fonts from '../styles/fonts';
 
 export function Confirmation(){
+    const navigation = useNavigation();
+
+    function handleMoveOn(){
+        navigation.navigate('PlantSelect');
+    }
+
+
     return (
         <SafeAreaView style={styles.container}>
             <View style={styles.content}>
@@ -19,7 +27,7 @@ export function Confirmation(){
                   plantinhas com muito cuidado.
                 </Text>
                 <View style={styles.footer}>
-                <Button title="Começar" />
+                <Button title="Começar" onPress={handleMoveOn} />
             </View>
             </View>
             
