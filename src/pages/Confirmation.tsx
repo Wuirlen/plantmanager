d3f49aeaa37a519bcc9b0b39e8876cb8b91ac5be
Wuirlen Azevedo <wuirlen.azevedo@gmail.com>
2021-05-03 00:35:02 -0400

    atualizando projeto

--- src/pages/Confirmation.tsx
+++ src/pages/Confirmation.tsx
@@ -1,15 +1,38 @@
 import React, { useState } from 'react';
-import { useNavigation } from '@react-navigation/core';
+import { useNavigation, useRoute } from '@react-navigation/core';
 import { SafeAreaView, StyleSheet, View, Text } from 'react-native';
 import { Button } from '../components/Button';
 import colors from '../styles/colors';
 import fonts from '../styles/fonts';
 
+interface Params {
+    title: string;
+    subtitle: string;
+    buttonTitle: string;
+    icon: 'smile' | 'hug';
+    nextScreen: string;
+}
+
+const emojis = {
+    hug: '🤗',
+    smile: '😄'
+}
+
 export function Confirmation(){
     const navigation = useNavigation();
+    const routes = useRoute();
+
+    const {
+        title,
+        subtitle,
+        buttonTitle,
+        icon,
+        nextScreen
+    } = routes.params as Params;
+
 
     function handleMoveOn(){
-        navigation.navigate('PlantSelect');
+        navigation.navigate(nextScreen);
     }
 
 
@@ -17,17 +40,16 @@ export function Confirmation(){
         <SafeAreaView style={styles.container}>
             <View style={styles.content}>
                 <Text style={styles.emoji}>
-                  😁
+                  {emojis[icon]}
                 </Text>
                 <Text style={styles.title}>
-                  Prontinho
+                  {title}
                 </Text>
                 <Text style={styles.subtitle}>
-                  Agora vamos começar a cuidar das suas 
-                  plantinhas com muito cuidado.
+                  {subtitle}
                 </Text>
                 <View style={styles.footer}>
-                <Button title="Começar" onPress={handleMoveOn} />
+                <Button title={buttonTitle} onPress={handleMoveOn} />
             </View>
             </View>
             
